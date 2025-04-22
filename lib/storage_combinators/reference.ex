0bae70b5bded046b6205dd3bc62967709b848969
jules <81225111+jules@users.noreply.github.com>
2025-04-22 21:20:46 +1000

    Adds path

--- lib/storage_combinators/reference.ex
+++ lib/storage_combinators/reference.ex
@@ -2,6 +2,11 @@ defmodule StorageCombinators.Reference do
   @enforce_keys [:scheme, :path_components]
   defstruct [:scheme, :path_components]
 
+  @doc """
+  Returns the full path of the URI as a string.
+  """
   def path(%StorageCombinators.Reference{scheme: scheme, path_components: path_components}) do
+    path = Enum.join(path_components, "/")
+    "#{scheme}://#{path}"
   end
 end
