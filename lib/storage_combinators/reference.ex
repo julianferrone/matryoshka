f69d1ba3c70748cebe296ac15076022b1a3e855d
jules <81225111+jules@users.noreply.github.com>
2025-04-22 21:26:11 +1000

    Add reference  to StorageCombinators.Reference
    This is a helper function to create a new StorageCombinators.Reference struct from 2 strings: a scheme, and a path (made of forward-slash-delimited path segments)

--- lib/storage_combinators/reference.ex
+++ lib/storage_combinators/reference.ex
@@ -3,6 +3,14 @@ defmodule StorageCombinators.Reference do
   defstruct [:scheme, :path_components]
 
   @doc """
+  Creates a Reference.
+  """
+  def reference(scheme, path) do
+    path_components = String.split(path, "/")
+    %StorageCombinators.Reference{scheme: scheme, path_components: path_components}
+  end
+
+  @doc """
   Returns the full path of the URI as a string.
   """
   def path(%StorageCombinators.Reference{scheme: scheme, path_components: path_components}) do
