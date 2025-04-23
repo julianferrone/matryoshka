07a6b6664d608f9566e3305ee0ae6a6fc6dfffaf
jules <81225111+jules@users.noreply.github.com>
2025-04-23 22:34:29 +1000

    Create Client for Passthrough storage combinator

---
+++ lib/storage_combinators/passthrough/client.ex
@@ -0,0 +1,14 @@
+defmodule StorageCombinators.Passthrough.Client do
+  alias StorageCombinators.Client, as: ScClient
+  @server StorageCombinators.Passthrough.Server
+
+  def start_link(default) when is_list(default), do: ScClient.start_link(@server, default)
+
+  def get(ref), do: ScClient.get(@server, ref)
+
+  def put(ref, value), do: ScClient.put(@server, ref, value)
+
+  def patch(ref, value), do: ScClient.patch(@server, ref, value)
+
+  def delete(ref), do: ScClient.delete(@server, ref)
+end
