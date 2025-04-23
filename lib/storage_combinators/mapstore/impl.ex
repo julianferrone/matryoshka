78b81f7ce97190deda34e879dfc080d9c32a8663
jules <81225111+jules@users.noreply.github.com>
2025-04-23 22:30:23 +1000

    Update MapStore get/put/patch to use inner struct and implement the Storage protocol

--- lib/storage_combinators/mapstore/impl.ex
+++ lib/storage_combinators/mapstore/impl.ex
@@ -1,14 +1,29 @@
-defmodule StorageCombinators.MapStore.Impl do
-  alias StorageCombinators.Patch
+defmodule StorageCombinators.Mapstore.Impl do
+  @enforce_keys [:map]
+  defstruct [:map]
 
-  def get(map, ref), do: Map.get(map, ref)
-  def put(map, ref, value), do: Map.put(map, ref, value)
+  @type t :: %__MODULE__{
+          map: Map
+        }
 
-  def patch(map, ref, value) do
-    first = Map.get(map, ref)
-    patched = Patch.patch(first, value)
-    Map.replace(map, ref, patched)
+  def map_store(), do: map_store(Map.new())
+  def map_store(map), do: %__MODULE__{map: map}
+end
+
+defimpl StorageCombinators.Storage, for: StorageCombinators.Mapstore.Impl do
+  alias StorageCombinators.Mapstore.Impl
+
+  def get(%Impl{map: map}, ref) do
+    Map.get(map, ref)
   end
 
-  def delete(map, ref), do: Map.delete(map, ref)
+  def put(%Impl{map: map}, ref, value) do
+    inner = Map.put(map, ref, value)
+    Impl.map_store(inner)
+  end
+
+  def delete(%Impl{map: map}, ref) do
+    inner = Map.delete(map, ref)
+    Impl.map_store(inner)
+  end
 end
