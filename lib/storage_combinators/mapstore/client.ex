7cbb0d8f580e104f92c64fb660c17a08059b7bfe
jules <81225111+jules@users.noreply.github.com>
2025-04-23 22:33:17 +1000

    Rename Mapstore to Mapstore.Client
    I want to maintain the convention of Client = Wrapping genserver calls with functions, Server = GenServer wrapping business logic, Impl = business logic

---
+++ lib/storage_combinators/mapstore/client.ex
@@ -0,0 +1,14 @@
+defmodule StorageCombinators.MapStore.Client do
+  alias StorageCombinators.Client, as: ScClient
+  @server StorageCombinators.MapClient.Server
+
+  def start_link(default) when is_list(default), do: ScClient.start_link(@server, default)
+
+  def get(ref), do: ScClient.get(@server, ref)
+
+  def put(ref, value), do: ScClient.put(@server, ref, value)
+
+  def patch(ref, value), do: ScClient.patch(@server, ref, value)
+
+  def delete(ref), do: ScClient.delete(@server, ref)
+end
