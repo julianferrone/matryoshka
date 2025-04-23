4c4489b36010b24c3b9f4b94daa4503c389b2a58
jules <81225111+jules@users.noreply.github.com>
2025-04-23 18:59:33 +1000

    Split Passthrough modules into separate folder and files

--- lib/storage_combinators/passthrough/server.ex
+++ lib/storage_combinators/passthrough/server.ex
@@ -1,27 +1,3 @@
-defmodule StorageCombinators.Passthrough do
-  @server StorageCombinators.Passthrough.Server
-
-  def start_link(default) when is_list(default) do
-    GenServer.start_link(@server, default)
-  end
-
-  def get(ref) do
-    GenServer.call(@server, {:get, ref})
-  end
-
-  def put(ref, value) do
-    GenServer.cast(@server, {:put, ref, value})
-  end
-
-  def patch(ref, value) do
-    GenServer.cast(@server, {:patch, ref, value})
-  end
-
-  def delete(ref) do
-    GenServer.cast(@server, {:delete, ref})
-  end
-end
-
 defmodule StorageCombinators.Passthrough.Server do
   use GenServer
 
