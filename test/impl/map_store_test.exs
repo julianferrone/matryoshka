3e872434186f8a251d9cb7a127c69c245deb2ad1
jules <81225111+jules@users.noreply.github.com>
2025-04-24 17:01:40 +1000

    Adding more tests for MapStore

--- test/impl/map_store_test.exs
+++ test/impl/map_store_test.exs
@@ -25,4 +25,38 @@ defmodule StorageCombinatorsTest.Impl.MapStoreTest do
     # Assert
     assert store == new_store
   end
+
+  test "Putting an item into a MapStore then getting it returns the same value" do
+    # Arrange
+    store = MapStore.map_store()
+
+    # Act
+    store = Storage.put(store, 1, :one)
+    {_store, value} = Storage.get(store, 1)
+
+    assert :one == value
+  end
+
+  test "Deleting on an empty MapStore doesn't change the MapStore" do
+    # Arrange
+    store = MapStore.map_store()
+
+    # Act
+    new_store = Storage.delete(store, 1)
+
+    # Assert
+    assert store == new_store
+  end
+
+  test "Adding an item to an empty MapStore, then deleting it immediately, returns the empty MapStore" do
+    # Arrange
+    store = MapStore.map_store()
+
+    # Act
+    store_one = Storage.put(store, 1, :one)
+    store_two = Storage.delete(store_one, 1)
+
+    # Assert
+    assert store == store_two
+  end
 end
