5e4a8658a8ae77a23a46e911715845c7bf762e5b
jules <81225111+jules@users.noreply.github.com>
2025-04-26 19:40:16 +1000

    Add SwitchingStore implementation and unit tests
    SwitchingStore allows us to direct storage commands and queries to different underlying Storage implementations based on the first path segment, like "file/item" to a file store, and "map/item" to a MapStore.

---
+++ lib/storage_combinators/impl/switching_store.ex
@@ -0,0 +1,91 @@
+defmodule StorageCombinators.Impl.SwitchingStore do
+  alias StorageCombinators.Impl.SwitchingStore
+  alias StorageCombinators.Reference
+  import StorageCombinators.StorageCombinators, only: [is_storage!: 1]
+
+  @enforce_keys [:path_store_map]
+  defstruct @enforce_keys
+
+  @typedoc """
+  A struct that implements the StorageCombinators.Storage protocol.
+  """
+  @type impl_storage :: any()
+
+  @type t :: %__MODULE__{
+          path_store_map: %{String.t() => impl_storage()}
+        }
+
+  @spec switching_store(%{String.t() => impl_storage()} | impl_storage()) :: SwitchingStore.t()
+  def switching_store(path_store_map) when is_map(path_store_map) do
+    Map.values(path_store_map)
+    |> Enum.each(&is_storage!/1)
+
+    %__MODULE__{
+      path_store_map: path_store_map
+    }
+  end
+
+  alias __MODULE__
+
+  def split_reference(ref) do
+    [path_head | path_tail] = Reference.path_segments(ref)
+    path_tail = Enum.join(path_tail, "/")
+    {path_head, path_tail}
+  end
+
+  def choose_store(%SwitchingStore{path_store_map: path_store_map}, ref) do
+    {first, _rest} = split_reference(ref)
+    Map.fetch(path_store_map, first)
+  end
+
+
+  defimpl StorageCombinators.Storage do
+    def fetch(%SwitchingStore{path_store_map: path_store_map} = store, ref) do
+      with {:ok, sub_store} <- SwitchingStore.choose_store(store, ref) do
+        {first, rest_ref} = SwitchingStore.split_reference(ref)
+        {new_sub_store, value} = StorageCombinators.Storage.fetch(sub_store, rest_ref)
+        new_path_store_map = Map.put(path_store_map, first, new_sub_store)
+        new_store = SwitchingStore.switching_store(new_path_store_map)
+        {new_store, value}
+      else
+        :error -> {store, :error}
+      end
+    end
+
+    def get(%SwitchingStore{path_store_map: path_store_map} = store, ref) do
+      with {:ok, sub_store} <- SwitchingStore.choose_store(store, ref) do
+        {first, rest_ref} = SwitchingStore.split_reference(ref)
+        {new_sub_store, value} = StorageCombinators.Storage.get(sub_store, rest_ref)
+        new_path_store_map = Map.put(path_store_map, first, new_sub_store)
+        new_store = SwitchingStore.switching_store(new_path_store_map)
+        {new_store, value}
+      else
+        :error -> {store, :error}
+      end
+    end
+
+    def put(%SwitchingStore{path_store_map: path_store_map} = store, ref, value) do
+      with {:ok, sub_store} <- SwitchingStore.choose_store(store, ref) do
+        {first, rest_ref} = SwitchingStore.split_reference(ref)
+        new_sub_store = StorageCombinators.Storage.put(sub_store, rest_ref, value)
+        new_path_store_map = Map.put(path_store_map, first, new_sub_store)
+        new_store = SwitchingStore.switching_store(new_path_store_map)
+        new_store
+      else
+        :error -> store
+      end
+    end
+
+    def delete(%SwitchingStore{path_store_map: path_store_map} = store, ref) do
+      with {:ok, sub_store} <- SwitchingStore.choose_store(store, ref) do
+        {first, rest_ref} = SwitchingStore.split_reference(ref)
+        new_sub_store = StorageCombinators.Storage.delete(sub_store, rest_ref)
+        new_path_store_map = Map.put(path_store_map, first, new_sub_store)
+        new_store = SwitchingStore.switching_store(new_path_store_map)
+        new_store
+      else
+        :error -> store
+      end
+    end
+  end
+end
---
+++ test/impl/switching_store_test.exs
@@ -0,0 +1,69 @@
+defmodule StorageCombinatorsTest.Impl.SwitchingStoreTest do
+  use ExUnit.Case, async: true
+
+  alias StorageCombinators.Impl.SwitchingStore
+  alias StorageCombinators.Impl.MapStore
+  alias StorageCombinators.Storage
+
+  setup do
+    [
+      store:
+        SwitchingStore.switching_store(%{
+          "one" => MapStore.map_store(),
+          "two" => MapStore.map_store(%{"test" => :test})
+        })
+    ]
+  end
+
+  test "Get on SwitchingStore with empty substore returns nil", %{store: store} do
+    {_new_store, value} = Storage.get(store, "one/test")
+    assert value == nil
+  end
+
+  test "Get on SwitchingStore with value in substore returns value", %{store: store} do
+    {_new_store, value} = Storage.get(store, "two/test")
+    assert value == :test
+  end
+
+  test "Fetch on SwitchingStore with empty substore returns :error", %{store: store} do
+    {_new_store, value} = Storage.fetch(store, "one/test")
+    assert value == :error
+  end
+
+  test "Fetch on SwitchingStore with value in substore returns value", %{store: store} do
+    {_new_store, value} = Storage.fetch(store, "two/test")
+    assert value == {:ok, :test}
+  end
+
+  test "Putting value into SwitchingStore, then getting with same reference, returns same value",
+       %{store: store} do
+    new_store = Storage.put(store, "one/item", :item)
+    {_store, value} = Storage.get(new_store, "one/item")
+    assert value == :item
+  end
+
+  test "Putting value into SwitchingStore, then fetching with same reference, returns same value",
+       %{store: store} do
+    new_store = Storage.put(store, "one/item", :item)
+    {_store, value} = Storage.fetch(new_store, "one/item")
+    assert value == {:ok, :item}
+  end
+
+  test "Cannot get value from SwitchingStore after deleting", %{store: store} do
+    new_store = Storage.put(store, "one/item", :item)
+    {new_store, value} = Storage.get(new_store, "one/item")
+    assert value == :item
+    new_store = Storage.delete(new_store, "one/item")
+    {_new_store, value} = Storage.get(new_store, "one/item")
+    assert value == nil
+  end
+
+  test "Cannot Fetch value from SwitchingStore after deleting", %{store: store} do
+    new_store = Storage.put(store, "one/item", :item)
+    {new_store, value} = Storage.fetch(new_store, "one/item")
+    assert value == {:ok, :item}
+    new_store = Storage.delete(new_store, "one/item")
+    {_new_store, value} = Storage.fetch(new_store, "one/item")
+    assert value == :error
+  end
+end
