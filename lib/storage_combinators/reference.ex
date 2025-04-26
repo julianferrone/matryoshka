112ef973f3b81a4a73afd382bb48147fdf5e9e31
jules <81225111+jules@users.noreply.github.com>
2025-04-26 19:35:44 +1000

    Rename path_components to path_segments and fix implementation for BitString
    BitString is the correct type to implement for, not String.

--- lib/storage_combinators/reference.ex
+++ lib/storage_combinators/reference.ex
@@ -2,17 +2,17 @@ defprotocol StorageCombinators.Reference do
   @doc """
   Splits a Reference into the list of underlying path components.
   """
-  def path_components(reference)
+  def path_segments(reference)
 end
 
-defimpl StorageCombinators.Reference, for: String do
-  def path_components(reference) do
+defimpl StorageCombinators.Reference, for: BitString do
+  def path_segments(reference) do
     String.split(reference, "/")
   end
 end
 
 defimpl StorageCombinators.Reference, for: Atom do
-  def path_components(reference) do
+  def path_segments(reference) do
     [Atom.to_string(reference)]
   end
 end
