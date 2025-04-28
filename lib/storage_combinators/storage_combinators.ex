2f4349193f802d0540f452bc02c2ca13d91e0145
jules <81225111+jules@users.noreply.github.com>
2025-04-28 15:30:39 +1000

    Remove get_or_nil

--- lib/storage_combinators/storage_combinators.ex
+++ lib/storage_combinators/storage_combinators.ex
@@ -7,11 +7,4 @@ defmodule StorageCombinators.StorageCombinators do
   def is_storage!(storage) do
     Protocol.assert_impl!(Storage, storage.__struct__)
   end
-
-  def get_or_nil(value) do
-    case value do
-      {:ok, val} -> val
-      :error -> nil
-    end
-  end
 end
