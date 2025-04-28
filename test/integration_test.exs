a0deac59c2932691229b21a31694b0a283771ad2
jules <81225111+jules@users.noreply.github.com>
2025-04-28 17:33:35 +1000

    Change IntegrationTest to test from publicly facing module StorageCombinators

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -1,43 +1,43 @@
 defmodule StorageCombinatorsTest.IntegrationTest do
   use ExUnit.Case, async: true
 
-  alias StorageCombinators.Impl.MapStore
-  alias StorageCombinators.Client
+  import StorageCombinators
+  doctest StorageCombinators
 
   setup do
-    {:ok, storage_server} = MapStore.map_store() |> Client.start_link
+    {:ok, storage_server} = map_store() |> start_link
     {:ok, storage_server: storage_server}
   end
 
   test "Client gets nil from empty storage server" do
-    assert Client.get(1) == nil
+    assert get(1) == nil
   end
 
   test "Client fetches :error from empty storage server" do
-    assert Client.fetch(1) == {:error, {:no_ref, 1}}
+    assert fetch(1) == {:error, {:no_ref, 1}}
   end
 
   test "Client puts and gets same item from storage server" do
-    Client.put(1, :one)
-    assert Client.get(1) == :one
+    put(1, :one)
+    assert get(1) == :one
   end
 
   test "Client puts and fetches same item from storage server" do
-    Client.put(1, :one)
-    assert Client.fetch(1) == {:ok, :one}
+    put(1, :one)
+    assert fetch(1) == {:ok, :one}
   end
 
   test "Client gets nil after deleting item from storage server" do
-    Client.put(1, :one)
-    assert Client.get(1) == :one
-    Client.delete(1)
-    assert Client.get(1) == nil
+    put(1, :one)
+    assert get(1) == :one
+    delete(1)
+    assert get(1) == nil
   end
 
   test "Client fetches :error after deleting item from storage server" do
-    Client.put(1, :one)
-    assert Client.fetch(1) == {:ok, :one}
-    Client.delete(1)
-    assert Client.fetch(1) == {:error, {:no_ref, 1}}
+    put(1, :one)
+    assert fetch(1) == {:ok, :one}
+    delete(1)
+    assert fetch(1) == {:error, {:no_ref, 1}}
   end
 end
