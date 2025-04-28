5caa5406634344dd3e5762d1ff818f8e31b1619e
jules <81225111+jules@users.noreply.github.com>
2025-04-28 16:44:52 +1000

    Add docstrings to mapping_store, use struct key access instead of pattern matching on MappingStore struct

--- lib/storage_combinators/impl/mapping_store.ex
+++ lib/storage_combinators/impl/mapping_store.ex
@@ -23,6 +23,67 @@ defmodule StorageCombinators.Impl.MappingStore do
 
   @identity &Function.identity/1
 
+  @doc """
+  Wraps an inner store with a mapping layer.
+
+  The `mapping_store/2` function allows you to map values when reading from
+  or writing to the `inner` store. It accepts an `inner` store and an optional
+  list of options.
+
+  ## Parameters
+
+  * `inner` - The underlying store module or instance to wrap. Must implement
+    the `StorageCombinators.Storage` protocol.
+  * `opts` - (keyword list) Optional settings:
+
+    * `:map_to_store` - (function) A function `(value -> stored_value)` that
+      maps values *before* storing them. Defaults to `Function.identity/1`.
+    * `:map_retrieved` - (function) A function `(stored_value -> value)` that
+      maps values when retrieved (get/fetch) from the store. Defaults to
+      `Function.identity/1`.
+    * `:map_ref` - (function). A function that maps references
+      `(ref -> mapped_ref)` before using them to locate values. Defaults to
+      `Function.identity/1`.
+
+  If the mapping function aren't provided, values and references are passed
+  through unchanged.
+
+  ## Examples:
+
+  Mapping value with function when putting the value:
+
+      iex> alias StorageCombinators.Storage
+      iex> alias StorageCombinators.Impl.MapStore
+      iex> alias StorageCombinators.Impl.MappingStore
+      iex> store = MapStore.map_store() |> MappingStore.mapping_store([map_to_store: fn x -> x + 1 end])
+      iex> store = Storage.put(store, "one", 1)
+      iex> {store, value} = Storage.get(store, "one")
+      iex> value
+      2
+
+  Mapping value with function when retrieving (get/fetch) the value:
+
+      iex> alias StorageCombinators.Storage
+      iex> alias StorageCombinators.Impl.MapStore
+      iex> alias StorageCombinators.Impl.MappingStore
+      iex> store = MapStore.map_store() |> MappingStore.mapping_store([map_retrieved: fn x -> x - 1 end])
+      iex> store = Storage.put(store, "one", 1)
+      iex> {store, value} = Storage.get(store, "one")
+      iex> value
+      0
+
+  Mapping reference with function
+
+      iex> alias StorageCombinators.Storage
+      iex> alias StorageCombinators.Impl.MapStore
+      iex> alias StorageCombinators.Impl.MappingStore
+      iex> store = MapStore.map_store() |> MappingStore.mapping_store([map_ref: fn x -> Atom.to_string(x) end])
+      # Converts the atom to a String path internally
+      iex> store = Storage.put(store, :item, "item")
+      iex> {store, value} = Storage.get(store, :item)
+      iex> value
+      "item"
+  """
   def mapping_store(inner, opts \\ []) do
     is_storage!(inner)
     map_ref = Keyword.get(opts, :map_ref, @identity)
@@ -41,51 +102,51 @@ defmodule StorageCombinators.Impl.MappingStore do
 
   defimpl StorageCombinators.Storage do
     @spec fetch(MappingStore.t(), StorageCombinators.Reference) :: any()
-    def fetch(
-          %MappingStore{inner: inner, map_ref: map_ref, map_retrieved: map_retrieved} = store,
-          ref
-        ) do
-      {inner_new, value} = StorageCombinators.Storage.fetch(inner, map_ref.(ref))
+    def fetch(store, ref) do
+      {inner_new, value} =
+        StorageCombinators.Storage.fetch(
+          store.inner,
+          store.map_ref.(ref)
+        )
 
       store_new = %{store | inner: inner_new}
 
       value_new =
         case value do
-          {:ok, value} -> {:ok, map_retrieved.(value)}
+          {:ok, value} -> {:ok, store.map_retrieved.(value)}
           error -> error
         end
 
       {store_new, value_new}
     end
 
-    def get(
-          %MappingStore{inner: inner, map_ref: map_ref, map_retrieved: map_retrieved} = store,
-          ref
-        ) do
-      {inner_new, value} = StorageCombinators.Storage.get(inner, map_ref.(ref))
+    def get(store, ref) do
+      {inner_new, value} = StorageCombinators.Storage.get(store.inner, store.map_ref.(ref))
 
       store_new = %{store | inner: inner_new}
 
       value_new =
         case value do
           nil -> nil
-          value -> map_retrieved.(value)
+          value -> store.map_retrieved.(value)
         end
 
       {store_new, value_new}
     end
 
-    def put(
-          %MappingStore{inner: inner, map_ref: map_ref, map_to_store: map_to_store} = store,
-          ref,
-          value
-        ) do
-      inner_new = StorageCombinators.Storage.put(inner, map_ref.(ref), map_to_store.(value))
+    def put(store, ref, value) do
+      inner_new =
+        StorageCombinators.Storage.put(
+          store.inner,
+          store.map_ref.(ref),
+          store.map_to_store.(value)
+        )
+
       %{store | inner: inner_new}
     end
 
-    def delete(%MappingStore{inner: inner, map_ref: map_ref} = store, ref) do
-      inner_new = StorageCombinators.Storage.delete(inner, map_ref.(ref))
+    def delete(store, ref) do
+      inner_new = StorageCombinators.Storage.delete(store.inner, store.map_ref.(ref))
       %{store | inner: inner_new}
     end
   end
