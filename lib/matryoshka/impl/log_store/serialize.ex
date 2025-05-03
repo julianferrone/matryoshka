8ebebeebcf0e2acced3367df454393f242d51f2f
jules <81225111+jules@users.noreply.github.com>
2025-05-03 17:12:55 +1000

    Return relative offset when formatting lines so we can update the index

--- lib/matryoshka/impl/log_store/serialize.ex
+++ lib/matryoshka/impl/log_store/serialize.ex
@@ -5,33 +5,55 @@ defmodule Matryoshka.Impl.LogStore.Serialize do
   # ------------------------ Timestamp -----------------------
 
   def binary_timestamp() do
-    timestamp = System.system_time(Encoding.time_unit)
+    timestamp = System.system_time(Encoding.time_unit())
     <<timestamp::big-unsigned-integer-size(Encoding.timestamp_size())>>
   end
 
   # ----------------------- Formatting -----------------------
 
   def format_write_log_line(key, value) do
-    {key_size, key} = pack_key(key)
-    {value_size, value} = pack_value(value)
-    line = Enum.join([
-      Encoding.atom_write_binary(),
-      key_size,
-      value_size,
-      key,
-      value
-    ])
-    prepend_timestamp(line)
+    {key_size, key_size_data, key} = pack_key(key)
+    {value_size, value_size_data, value} = pack_value(value)
+
+    line =
+      Enum.join([
+        Encoding.atom_write_binary(),
+        key_size_data,
+        value_size_data,
+        key,
+        value
+      ])
+
+    relative_offset =
+      Enum.sum([
+        Encoding.bits_to_bytes(Encoding.timestamp_size()),
+        Encoding.bits_to_bytes(Encoding.key_size()),
+        Encoding.bits_to_bytes(Encoding.value_size()),
+        key_size,
+        value_size
+      ])
+
+    {prepend_timestamp(line), relative_offset}
   end
 
   def format_delete_log_line(key) do
-    {key_size, key} = pack_key(key)
-    line = Enum.join([
-      Encoding.atom_delete_binary(),
-      key_size,
-      key
-    ])
-    prepend_timestamp(line)
+    {key_size, key_size_data, key} = pack_key(key)
+
+    line =
+      Enum.join([
+        Encoding.atom_delete_binary(),
+        key_size_data,
+        key
+      ])
+
+    relative_offset =
+      Enum.sum([
+        Encoding.bits_to_bytes(Encoding.timestamp_size()),
+        Encoding.bits_to_bytes(Encoding.key_size()),
+        key_size
+      ])
+
+    {prepend_timestamp(line), relative_offset}
   end
 
   def prepend_timestamp(data) when is_binary(data) do
@@ -42,8 +64,9 @@ defmodule Matryoshka.Impl.LogStore.Serialize do
   @spec pack_term(term(), integer()) :: {binary(), binary()}
   def pack_term(term, int_size) do
     binary = term |> term_to_binary()
-    size_data = <<byte_size(binary)::big-unsigned-integer-size(int_size)>>
-    {size_data, binary}
+    size = byte_size(binary)
+    size_data = <<size::big-unsigned-integer-size(int_size)>>
+    {size, size_data, binary}
   end
 
   def pack_key(key), do: pack_term(key, Encoding.key_size())
