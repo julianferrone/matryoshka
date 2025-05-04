dd7db62c4828f1777708b029fae745504cf35912
jules <81225111+jules@users.noreply.github.com>
2025-05-04 19:42:34 +1000

    Use struct access syntax instead of pattern matching in PassThrough

--- lib/matryoshka/impl/pass_through.ex
+++ lib/matryoshka/impl/pass_through.ex
@@ -21,25 +21,25 @@ defmodule Matryoshka.Impl.PassThrough do
   alias __MODULE__
 
   defimpl Matryoshka.Storage do
-    def fetch(%PassThrough{inner: inner}, ref) do
-      {new_inner, value} = Matryoshka.Storage.fetch(inner, ref)
+    def fetch(store, ref) do
+      {new_inner, value} = Matryoshka.Storage.fetch(store.inner, ref)
       store = PassThrough.pass_through(new_inner)
       {store, value}
     end
 
-    def get(%PassThrough{inner: inner}, ref) do
-      {new_inner, value} = Matryoshka.Storage.get(inner, ref)
+    def get(store, ref) do
+      {new_inner, value} = Matryoshka.Storage.get(store.inner, ref)
       store = PassThrough.pass_through(new_inner)
       {store, value}
     end
 
-    def put(%PassThrough{inner: inner}, ref, value) do
-      new_inner = Matryoshka.Storage.put(inner, ref, value)
+    def put(store, ref, value) do
+      new_inner = Matryoshka.Storage.put(store.inner, ref, value)
       PassThrough.pass_through(new_inner)
     end
 
-    def delete(%PassThrough{inner: inner}, ref) do
-      new_inner = Matryoshka.Storage.delete(inner, ref)
+    def delete(store, ref) do
+      new_inner = Matryoshka.Storage.delete(store.inner, ref)
       PassThrough.pass_through(new_inner)
     end
   end
