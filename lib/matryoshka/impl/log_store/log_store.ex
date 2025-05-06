117e2eab24951758e6056ce5186b45b491f5f6df
jules <81225111+jules@users.noreply.github.com>
2025-05-06 22:31:38 +1000

    Add docstrings to LogStore

--- lib/matryoshka/impl/log_store/log_store.ex
+++ lib/matryoshka/impl/log_store/log_store.ex
@@ -18,18 +18,38 @@ defmodule Matryoshka.Impl.LogStore do
           index: map()
         }
 
+  @doc """
+  Opens a log file, either loading its offset index or creating a new one if
+  the file doesn't exist.
+
+  The function attempts to open the log file at the given `log_filepath`.
+
+  If the file exists, it loads the offset index using `Deserialize.load_offsets/1`
+  and opens the file for both reading and writing. If the file doesn't exist,
+  it creates a new index and opens the file for both reading and writing.
+
+  Returns a `%__MODULE__{}` struct containing the following:
+    - `reader`: The file descriptor for reading from the log file.
+    - `writer`: The file descriptor for writing to the log file.
+    - `index`: A map representing the offset index of the log file.
+
+  If the log file doesn't exist, the index is initialized as an empty map.
+  """
   def log_store(log_filepath) do
-    {reader, writer, index} = case File.open(log_filepath, [:binary, :read]) do
-      {:ok, reader} ->
-        index = Deserialize.load_offsets(reader)
-        {:ok, writer} = File.open(log_filepath, [:binary, :write])
-        {reader, writer, index}
-      {:error, _reason} ->
-        {:ok, writer} = File.open(log_filepath, [:binary, :write])
-        {:ok, reader} = File.open(log_filepath, [:binary, :read])
-        index = Map.new()
-        {reader, writer, index}
-    end
+    {reader, writer, index} =
+      case File.open(log_filepath, [:binary, :read]) do
+        {:ok, reader} ->
+          index = Deserialize.load_offsets(reader)
+          {:ok, writer} = File.open(log_filepath, [:binary, :write])
+          {reader, writer, index}
+
+        {:error, _reason} ->
+          {:ok, writer} = File.open(log_filepath, [:binary, :write])
+          {:ok, reader} = File.open(log_filepath, [:binary, :read])
+          index = Map.new()
+          {reader, writer, index}
+      end
+
     %__MODULE__{reader: reader, writer: writer, index: index}
   end
 
