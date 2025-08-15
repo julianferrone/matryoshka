e7d8a0b11a602c43d953ea1e708996250eb8245f
Julian Ferrone <hi@julianferrone.com>
2025-08-15 13:28:35 +1000

    Create SFTP store implementation

---
+++ lib/matryoshka/impl/sftp_store.ex
@@ -0,0 +1,54 @@
+defmodule Matryoshka.Impl.SftpStore do
+  @enforcekeys [:pid, :connection]
+  defstruct [:pid, :connection]
+
+  @type t :: %__MODULE__{
+          pid: pid()
+        }
+
+  def sftp_store(host, username, password) do
+    host = String.to_charlist(host)
+    username = String.to_charlist(username)
+    password = String.to_charlist(password)
+
+    {:ok, pid, connection} = :ssh_sftp.start_channel(
+      host,
+      [
+        {:user, username},
+        {:password, password}
+      ]
+    )
+    %__MODULE__{pid: pid, connection: connection}
+  end
+
+  alias __MODULE__
+
+  defimpl Matryoshka.Storage do
+    def fetch(store, ref) do
+      ref = String.to_charlist(ref)
+      value = :ssh_sftp.read_file(store.pid, ref)
+      {store, value}
+    end
+
+    def get(store, ref) do
+      ref = String.to_charlist(ref)
+      value = case :ssh_sftp.read_file(store.pid, ref) do
+        {:ok, value} -> {:ok, value}
+        {:error, reason} -> :nil
+      end
+      {store, value}
+    end
+
+    def put(store, ref, value) do
+      ref = String.to_charlist(ref)
+      :ssh_sftp.write_file(store.pid, ref, value)
+      store
+    end
+
+    def delete(store, ref) do
+      ref = String.to_charlist(ref)
+      :ssh_sftp.delete(store.pid, ref)
+      store
+    end
+  end
+end
