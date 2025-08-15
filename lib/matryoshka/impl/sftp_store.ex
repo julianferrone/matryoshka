0d3922c37a3953d1fc8bd9da63268595b39846f2
Julian Ferrone <hi@julianferrone.com>
2025-08-15 22:22:35 +1000

    Update SftpStore to silently accept hosts, take a port for connection as well, do better error transformation from :ssh_sftp to the expected Matryoshka shapes for get and fetch

--- lib/matryoshka/impl/sftp_store.ex
+++ lib/matryoshka/impl/sftp_store.ex
@@ -1,5 +1,5 @@
 defmodule Matryoshka.Impl.SftpStore do
-  @enforcekeys [:pid, :connection]
+  @enforce_keys [:pid, :connection]
   defstruct [:pid, :connection]
 
   @type t :: %__MODULE__{
@@ -7,46 +7,69 @@ defmodule Matryoshka.Impl.SftpStore do
           connection: :ssh.connection_ref()
         }
 
-  def sftp_store(host, username, password) do
+  def sftp_store(host, port, username, password) do
     username = String.to_charlist(username)
     password = String.to_charlist(password)
 
     :ssh.start()
-    {:ok, pid, connection} = :ssh_sftp.start_channel(
-      host,
-      [
-        {:user, username},
-        {:password, password}
-      ]
-    )
+
+    {:ok, pid, connection} =
+      :ssh_sftp.start_channel(
+        host,
+        port,
+        [
+          silently_accept_hosts: true,
+          user: username,
+          password: password
+        ]
+      )
+
     %__MODULE__{pid: pid, connection: connection}
   end
 
   defimpl Matryoshka.Storage do
     def fetch(store, ref) do
-      ref = String.to_charlist(ref)
-      value = :ssh_sftp.read_file(store.pid, ref)
+      value =
+        case :ssh_sftp.read_file(
+               store.pid,
+               String.to_charlist(ref)
+             ) do
+          {:ok, bin} -> {:ok, :erlang.binary_to_term(bin)}
+          {:error, :no_such_file} -> {:error, {:no_ref, ref}}
+          {:error, other} -> {:error, other}
+        end
+
       {store, value}
     end
 
     def get(store, ref) do
       ref = String.to_charlist(ref)
-      value = case :ssh_sftp.read_file(store.pid, ref) do
-        {:ok, value} -> {:ok, value}
-        {:error, _reason} -> :nil
-      end
+
+      value =
+        case :ssh_sftp.read_file(store.pid, ref) do
+          {:ok, bin} -> :erlang.binary_to_term(bin)
+          {:error, _reason} -> nil
+        end
+
       {store, value}
     end
 
     def put(store, ref, value) do
-      ref = String.to_charlist(ref)
-      :ssh_sftp.write_file(store.pid, ref, value)
+      :ssh_sftp.write_file(
+        store.pid,
+        String.to_charlist(ref),
+        :erlang.term_to_binary(value)
+      )
+
       store
     end
 
     def delete(store, ref) do
-      ref = String.to_charlist(ref)
-      :ssh_sftp.delete(store.pid, ref)
+      :ssh_sftp.delete(
+        store.pid,
+        String.to_charlist(ref)
+      )
+
       store
     end
   end
